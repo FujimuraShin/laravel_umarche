47b315babd72828d48a63fa6ffa882294c6f8153
FujimuraShin <sfujimura2@gmail.com>
2021-11-24 14:42:59 +0900

    名前つきスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">ヘッダー001</x-slot>
 コンポーネント001
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+<x-slot name="header">ヘッダー002</x-slot>
 コンポーネント002
 </x-tests.app>
