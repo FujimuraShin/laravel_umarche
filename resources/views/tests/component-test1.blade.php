cdc6f9bd188504194897a118fee9c3529017eb32
FujimuraShin <sfujimura2@gmail.com>
2021-11-24 14:59:33 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,6 @@
     <x-slot name="header">ヘッダー001</x-slot>
     コンポーネント001
 
-    <x-tests.card title="タイトル" content="本文"/>
-    
+    <x-tests.card title="タイトル" content="本文" :message="$message"/>
+
 </x-tests.app>
