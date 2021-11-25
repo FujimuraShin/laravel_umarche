8b072e515bba78140112cfe7b0c158a23e292cc6
FujimuraShin <sfujimura2@gmail.com>
2021-11-25 09:52:59 +0900

    Props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,7 @@
+@props(['title'=>'タイトル初期値',
+    'message'=>'メッセージ初期値'
+        ,'content'=>'本文初期値'])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,5 @@
     コンポーネント001
 
     <x-tests.card title="タイトル" content="本文" :message="$message"/>
-
+    <x-tests.card title="タイトル001-2"/>
 </x-tests.app>
